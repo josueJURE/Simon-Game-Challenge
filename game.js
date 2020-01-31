90cbecf0b8282371efa6a07c824047f4b82d07da
josueJURE <57095041+josueJURE@users.noreply.github.com>
2020-01-31 18:26:28 +0000

    Added initial Simon-Game-Challenge files

---
+++ game.js
@@ -0,0 +1,14 @@
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+
+function nextSequence() {
+
+  var randomNumber = Math.floor(Math.random()*4)
+
+  var randomChosenColor = buttonColours[randomNumber];
+
+  gamePattern.push(randomChosenColor)
+
+}
